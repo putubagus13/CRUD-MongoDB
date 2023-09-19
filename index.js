96cb12ee141ce6dc0bde82a3b6d39ec97bfc2161
putubagus13 <putu031216@gmail.com>
2023-09-19 19:16:04 +0700

    fix: app get

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const express = require("express")
 const app = express()
 const monggoose = require("mongoose")
 app.use(express.json());
-// const bodyParser = require("body-parser")
-
-// app.use(bodyParser.json({ type: 'application/*+json' }))
 
 app.use("/users", require("./src/routers/index") )
+app.get("/", (req, res) => {
+    return res.json({
+      success: true,
+      message: "backend is runing well",
+    });
+  });
 
 monggoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
